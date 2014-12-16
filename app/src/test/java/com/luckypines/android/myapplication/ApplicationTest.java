8212e9afc397fecb7b37cbd2b38a634b63d6a4f3
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2014-12-15 23:10:29 -0500

    Emulate SDK 18 to solve the error.

--- app/src/test/java/com/luckypines/android/myapplication/ApplicationTest.java
+++ app/src/test/java/com/luckypines/android/myapplication/ApplicationTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(emulateSdk = 18)
 public class ApplicationTest {
     @Test
     public void shouldHaveApplicationName() throws Exception {
