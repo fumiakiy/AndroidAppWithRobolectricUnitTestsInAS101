cb2bf6e5a84f775ebd1b8f203061cbb77b30c8b5
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2014-12-16 12:20:56 -0500

    This fixes the error "android.content.res.Resources$NotFoundException: Resource ID #0x7f0c0000". `./gradlew test` runs successfully.

--- test/src/test/java/com/luckypines/android/myapplication/test/ApplicationTest.java
+++ test/src/test/java/com/luckypines/android/myapplication/test/ApplicationTest.java
@@ -17,7 +17,7 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(emulateSdk = 18, libraries = {"../../build/intermediates/exploded-aar/com.android.support/appcompat-v7/21.0.3"}, manifest = "../app/src/main/AndroidManifest.xml")
+@Config(reportSdk = 18, emulateSdk = 18, qualifiers = "v11", libraries = {"../../build/intermediates/exploded-aar/com.android.support/appcompat-v7/21.0.3"}, manifest = "../app/src/main/AndroidManifest.xml")
 public class ApplicationTest {
     @Test
     public void shouldHaveApplicationName() throws Exception {
