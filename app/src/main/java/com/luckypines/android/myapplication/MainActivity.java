7f955ecd19f478a67ae468b0aa6df7984025ec03
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2014-12-15 19:25:35 -0500

    Button action implemented.

--- app/src/main/java/com/luckypines/android/myapplication/MainActivity.java
+++ app/src/main/java/com/luckypines/android/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onButton1Click(View view) {
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setText(R.string.nice_click);
     }
 }
