f1da2b5f75c56a86f31496ca4bf0ddd46c6425da
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2014-12-15 23:28:07 -0500

    Use AssertJ for Android in a test. It fails without "androidTestCompile project.files..." with an error "error: package org.assertj.android.api does not exist import org.assertj.android.api.Assertions;" and "error: cannot find symbol Assertions.assertThat(textView1).hasText(R.string.hello_world);"

--- app/src/test/java/com/luckypines/android/myapplication/ApplicationTest.java
+++ app/src/test/java/com/luckypines/android/myapplication/ApplicationTest.java
@@ -1,6 +1,9 @@
 package com.luckypines.android.myapplication;
 
 
+import android.widget.TextView;
+
+import org.assertj.android.api.Assertions;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,5 +19,11 @@ public class ApplicationTest {
     public void shouldHaveApplicationName() throws Exception {
         String appName = Robolectric.getShadowApplication().getString(R.string.app_name);
         Assert.assertThat(appName, CoreMatchers.equalTo("My Application"));
+    }
+
+    public void defaultTextViewHasHelloWorld() throws Exception {
+        MainActivity mainActivity = Robolectric.buildActivity(MainActivity.class).create().start().resume().get();
+        TextView textView1 = (TextView)mainActivity.findViewById(R.id.textView1);
+        Assertions.assertThat(textView1).hasText(R.string.hello_world);
     }
 }
